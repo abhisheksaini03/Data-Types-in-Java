207a0155154bd92a66b85c31d5e216ba8ce603f2
Abhishek saini <abhisheksaini03april@gmail.com>
2024-07-19 12:39:39 +0530

    this is my change

--- CharDataType.java
+++ CharDataType.java
@@ -4,6 +4,11 @@
 public class CharDataType {
 
     public static void main(String[] args) {
+        char my_char1 = 'A';
+        char my_char2 = 'B';
+        System.out.println("my_char1 : "+ my_char1 );
+        System.out.println("my_char2 : "+ my_char2);
+        System.out.println("after increment my_char2 " + (char)(my_char2+1));
         
     }
 }
