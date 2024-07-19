fc76e243bf212b2b348c5f35f3d1ee5490485af7
Abhishek saini <abhisheksaini03april@gmail.com>
2024-07-19 23:28:37 +0530

    hello

--- CharDataType.java
+++ CharDataType.java
@@ -9,6 +9,11 @@ public class CharDataType {
         System.out.println("my_char1 : "+ my_char1 );
         System.out.println("my_char2 : "+ my_char2);
         System.out.println("after increment my_char2 " + (char)(my_char2+1));
+
+        char my_char3 = '&';
+        System.out.println("my_char3 : " + my_char3);
+        char my_char4 ='1';
+        System.out.println("my_char4 :" + my_char4);
         
     }
 }
