cd4d6b706bec59cf67fac7a520ec112dd665ba65
Abhishek saini <abhisheksaini03april@gmail.com>
2024-07-21 10:45:38 +0530

    hello

--- CharDataType.java
+++ CharDataType.java
@@ -14,6 +14,46 @@ public class CharDataType {
         System.out.println("my_char3 : " + my_char3);
         char my_char4 ='1';
         System.out.println("my_char4 :" + my_char4);
+
+        char my_char5 = 'अ';
+        System.out.println(my_char5);
+
+        String hindi = "कृपया मान दर्ज करें" ;
+        System.out.println(hindi);
         
+        
+        // boolean datatype
+
+        int a =9;
+        boolean b = a<10;
+        System.out.println(b);
+
+        if(false)
+        {
+            System.out.println("hello");
+        }
+        else
+        {
+            System.out.println("World");
+        }
+
+        // example  of boolean data type
+        int c=10,d=10,f=10;
+        boolean g = (10>5) == (11<5); 
+        System.out.println(g);
+
+        // example of type casting uin boolean data type
+        int a_int = 10;
+        boolean a_bool =(boolean)(a_int);
+        int b_int =1;
+        boolean b_bool = (boolean) (b_int);
+
+
+        // integer datatype
+
+
+
+
+
     }
 }
